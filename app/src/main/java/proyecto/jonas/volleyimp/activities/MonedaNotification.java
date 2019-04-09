8f7b71a3f6f6f789c05b3c57bb58825d47f2f912
Jonas Lopez <jonas_lopez@intranet.itresources>
2019-04-09 20:47:03 -0300

    inicializo UI para testear informacion

--- app/src/main/java/proyecto/jonas/volleyimp/activities/MonedaNotification.java
+++ app/src/main/java/proyecto/jonas/volleyimp/activities/MonedaNotification.java
@@ -2,6 +2,8 @@ package proyecto.jonas.volleyimp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import proyecto.jonas.volleyimp.R;
@@ -13,24 +15,38 @@ public class MonedaNotification extends AppCompatActivity {
     TextView tvCompra;
     TextView tvVenta;
     TextView tvMoneda;
+    Button btnAceptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moneda_notification);
 
-        initialize();
+        initUI();
 
-
+        initProperties();
     }
 
-    private void initialize() {
+    private void initProperties() {
         Moneda monedaItem = (Moneda) getIntent().getExtras().get(MonedasConstant.ITEM_MONEDA);
 
+        tvCompra.setText(monedaItem.getCompraValue());
+        tvVenta.setText(monedaItem.getVentaValue());
+        tvMoneda.setText(monedaItem.getMonedaName());
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String holis = "";
+            }
+        });
+    }
+
+    private void initUI() {
         tvMoneda = findViewById(R.id.textViewMoneda);
+        tvCompra = findViewById(R.id.textViewCompra);
         tvVenta  = findViewById(R.id.textViewVenta);
         tvMoneda = findViewById(R.id.textViewMoneda);
-
-
+        btnAceptar = findViewById(R.id.btnAceptar);
     }
 }
